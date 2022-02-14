dbe9ccfa8d7fd43200bda31f5b7abb2b9557b4c1
Ariel Smith <arieljeansmith@gmail.com>
2022-02-14 15:32:08 -0800

    Lesson 6 of tutorial complete

--- main.js
+++ main.js
@@ -27,6 +27,24 @@ var paddleX = (canvas.width-paddleWidth) / 2;
 var rightPressed = false;
 var leftPressed = false;
 
+// Brick variables
+var brickRowCount = 3;
+var brickColumnCount = 5;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
+
+// Handling the brick array
+var bricks = [];
+for(var c=0; c<brickColumnCount; c++) {
+    bricks[c] = [];
+    for(var r=0; r<brickRowCount; r++) {
+        bricks[c][r] = { x: 0, y: 0 };
+    }
+}
+
 
 // ==========================================
 // K E Y U P / D O W N  E V E N T  L I S T E N E R S
@@ -82,13 +100,31 @@ function drawPaddle() {
     ctx.closePath();
 }
 
+// Function to draw the bricks
+function drawBricks() {
+    for(var c=0; c<brickColumnCount; c++) {
+        for(var r=0; r<brickRowCount; r++) {
+            var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+            var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+            bricks[c][r].x = brickX;
+            bricks[c][r].y = brickY;
+            ctx.beginPath();
+            ctx.rect(brickX, brickY, brickWidth, brickHeight);
+            ctx.fillStyle = "#0095DD";
+            ctx.fill();
+            ctx.closePath();
+        }
+    }
+}
+
 // Function to clear the canvas, draw the ball calling the drawBall
 // function, and get the ball moving
 function draw() {
     // Clear the canvas so movement doesn't leave a trail
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // Draw the ball and paddle
+    // Draw the ball, paddle, and bricks
+    drawBricks();
     drawBall();
     drawPaddle();
 
